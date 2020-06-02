82e27ab52b89f0de42c8f59f950fddf78ebc718a
Fran <franciscodumont1@gmail.com>
2020-06-01 22:57:30 -0300

    :tv: Polybar in multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
-#polybar bot &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."
